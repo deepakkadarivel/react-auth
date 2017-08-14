edce172995a3e913eae44c034a7e99a05457ed34
Deepakkv <deepakkv@in-deepakkv.local>
2017-08-14 21:42:29 +0530

    [Deepak] Googleplus login

--- src/components/auth/config.js
+++ src/components/auth/config.js
@@ -1,5 +1,4 @@
 export default {
     facebook: "112930042684279",
-    linkedin: "", //Linkedin app id
-    google  : "846832770923-nq1asqrnbgordka380aee3fu0th7kk7l.apps.googleusercontent.com" //Google client id  (7687687687-nq1asqrnbgordksqerer23dewqqe.apps.googleusercontent.com)
+    google  : "148641159366-ctm99rs15qu1s60tr037kcogbijqnemk.apps.googleusercontent.com"
 }
